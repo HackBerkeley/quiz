5aebfc3b834132645a2d5bcfdaca59a2784b2540
ericz <really.ez@gmail.com>
2011-10-27 14:03:46 -0700

    init

---
+++ public/main.js
@@ -0,0 +1,72 @@
+var timeLeft = 90;
+var timer;
+
+now.correct = function(){ 
+  modal('Nice you got it right!'); 
+}
+
+function modal(msg) {
+  $('#modal #text').text(msg);
+  $('#modal').show();
+}
+
+function hideModal() {
+  $('#modal').hide();
+}
+
+
+var wrong =  ['You\'re doing it wrong. Stop doing it wrong.','Keyboard error: please type the right letters.','Error 0xDEADBEEF : Unknown fatal error','That answer is against my programming.','Error: There is an error in your answer.','You can\'t have your wrong answer and eat it too.','Your answer is beyond help.','Your answer will not receive sponsorship from Flotype.','Kernel panic! Man all lifeboats! Women and children first!','#f','There are wrong answers. Yours is one.','0','No.','Not enough memory to display the error m'];
+var wrongI = 0;
+
+now.wrongAns = function(){
+    if(wrongI >= wrong.length) {
+      wrongI == 0;
+    } else {
+      wrongI++;
+    }
+    $('#result').html(wrong[wrongI]); 
+    $("#result").stop().css({opacity: 0}).animate({opacity: 1});
+}
+
+
+function refreshTime(){
+    $('.time').html(timeLeft);
+}
+
+now.declareWinner = function(place, name, id){
+   if(place == 1) {
+    var msg = $("<p>"+name+" just placed 1st!</p>");    
+   } else if(place == 2) {
+    var msg = $("<p>"+name+" just got 2nd</p>");   
+   } else if(place == 3) {
+    var msg = $("<p>"+name+" just placed 3rd</p>");
+   }
+   $('#result').html(msg); 
+   $("#result").stop().css({opacity: 0}).animate({opacity: 1});
+}
+
+
+now.disable = function(){
+    clearInterval(timer);
+    modal('Rounds over');
+}
+
+now.broadcast = function(question, time){
+  clearInterval(timer);
+  $("#result").css({opacity: 0});
+  $("#answer").val('');
+  hideModal();
+  timeLeft = time;
+  refreshTime();
+  timer = setInterval(function(){timeLeft--; refreshTime();}, 1000);
+}
+
+$(document).ready(function(){
+  $("#answer").keypress(function(e){
+    if(e.which === 13) {
+      now.guess($(this).val());
+      $(this).val('');
+    }
+  });
+  now.name = 'asdf'+ (new Date() - 0);
+});
---
+++ server.js
@@ -0,0 +1,101 @@
+require.paths.unshift('./node_modules')
+
+var nowjs = require('now');
+var express = require('express');
+var fs = require('fs');
+var app =  express.createServer();
+
+var mongo = require('mongoskin');
+
+var timer = 0; // javascript timer
+var currentQ = -1; // index
+
+
+var listQ = [
+  {question: 'what is 3+3', answer: ['6', 'six'], time: 30},
+  {question: 'what is 4+4', answer: ['9', 'eight'], time: 30},
+  {question: 'what is 5+5', answer: ['10', 'ten'], time: 30},
+  {question: 'what is 6+6', answer: ['12', 'twelve'], time: 30},
+]; // array of questions
+
+mongo.db('heroku:hackers@staff.mongohq.com:10065/app1491090').collection('quiz').find().toArray(function(err, items){
+    listQ = items;
+});
+
+var scores = {}; // unique userids to objects which contain questionids mapped to scores.
+var winners = [];
+var timeLeft;
+var intervalTimer = 0;
+
+// string question, keywords that answer the question (array), time (int), optional key field (project, order, etc).
+
+// Initialize main server
+app.use(express.bodyParser());
+
+app.use(express.static(__dirname + '/public'));
+ 
+
+app.listen(process.env.PORT || 8082);
+
+var everyone = nowjs.initialize(app);
+
+everyone.now.next = function(i) {
+  clearTimeout(timer);
+  clearInterval(intervalTimer);
+  
+  if(i !== undefined) {
+    currentQ = i;
+  } else {
+    currentQ += 1;
+  }
+  console.log(currentQ);
+  var current = listQ[currentQ];
+  everyone.now.broadcast(current.question, current.time);
+  winners = [];
+  timer = setTimeout(function () { everyone.now.disable(); clearInterval(intervalTimer); }, current.time * 1000);
+  timeLeft = current.time;
+  intervalTimer = setInterval(function () { timeLeft -= 1; }, 1000);
+  this.now.nextQ(current.question, currentQ);
+}
+
+everyone.now.guess = function(g) {
+  var current = listQ[currentQ];
+  for (var i in current.answer) {
+    if (g.indexOf(current.answer[i]) > -1) {
+      if (winners.length < 3) {
+        someoneWon(this);
+      }
+      return true;
+    }
+  }
+  this.now.wrongAns();
+}
+
+function updateDash () {
+  var data = {};
+  for(var id in scores) {
+    data[id] = 0;
+    for(var j in scores[id]) {
+      data[id] += scores[id][j];
+    }
+  }
+  everyone.now.updateTable(data);
+}
+
+someoneWon = function(self) {
+  winners.push(self);
+  var place = winners.length;
+  if(!scores[self.now.name]) {
+    scores[self.now.name] = {};
+  }
+  scores[self.now.name][listQ[currentQ].question] = timeLeft + 20;
+  everyone.now.declareWinner(place, self.now.name, self.user.clientId);
+  updateDash();
+  self.now.correct();
+  if (place == 3) {
+    clearInterval(intervalTimer);
+    clearTimeout(timer);
+    everyone.now.disable();
+  }
+}
+
